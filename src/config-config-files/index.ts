5413cfa634178bc86f6dbbb4cd8719fdcfc64245
Dom Porada <dom@dom.engineering>
2026-02-11 11:39:31 +0100

    Disable `import/no-unassigned-import` in config files

--- src/config-config-files/index.ts
+++ src/config-config-files/index.ts
@@ -3,6 +3,7 @@ import type { LinterConfigOverrideEntry } from '../types/index.d.ts';
 const config: LinterConfigOverrideEntry = {
 	rules: {
 		'import/no-anonymous-default-export': ['error', { allowObject: true }],
+		'import/no-unassigned-import': 'off',
 		'import/unambiguous': 'off',
 		'unicorn/no-empty-file': 'off',
 	},

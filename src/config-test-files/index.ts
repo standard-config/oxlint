7cfbdefc2fb0e9e9a6ee1ef5635553a26de1d40e
Dom Porada <dom@dom.engineering>
2026-02-27 22:47:46 +0100

    Warn on `promise/no-promise-in-callback`

--- src/config-test-files/index.ts
+++ src/config-test-files/index.ts
@@ -69,6 +69,7 @@ const config: LinterConfigOverrideEntry = {
 		'oxc/approx-constant': 'off',
 		'oxc/no-accumulating-spread': 'off',
 		'oxc/no-map-spread': 'off',
+		'promise/no-promise-in-callback': 'off',
 		'promise/no-return-wrap': 'off',
 		'promise/prefer-await-to-then': 'off',
 		'react-perf/jsx-no-new-array-as-prop': 'off',

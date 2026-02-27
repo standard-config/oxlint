97d46d5186da62e57bb278753a7ab5d5766662aa
Dom Porada <dom@dom.engineering>
2026-02-27 22:55:44 +0100

    Warn on `promise/catch-or-return`

--- src/config-test-files/index.ts
+++ src/config-test-files/index.ts
@@ -69,6 +69,7 @@ const config: LinterConfigOverrideEntry = {
 		'oxc/approx-constant': 'off',
 		'oxc/no-accumulating-spread': 'off',
 		'oxc/no-map-spread': 'off',
+		'promise/catch-or-return': 'off',
 		'promise/no-callback-in-promise': 'off',
 		'promise/no-promise-in-callback': 'off',
 		'promise/no-return-wrap': 'off',

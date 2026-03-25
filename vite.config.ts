24affddd4af69658a996328517feb44268002467
Dom Porada <dom@dom.engineering>
2026-03-25 09:16:12 +0100

    Update dependencies

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import {
+	oxlintConfigBase,
+	oxlintConfigConfigFiles,
+} from '@standard-config/eslint/utilities';
+import { defineConfig } from 'vite-plus';
+import { defineOxlintConfig } from './src/index.ts';
+
+export default defineConfig({
+	test: {
+		typecheck: {
+			enabled: true,
+		},
+	},
+	lint: defineOxlintConfig(oxlintConfigBase, {
+		overrides: [
+			{
+				files: ['src/config-*/index.ts'],
+				...oxlintConfigConfigFiles,
+			},
+		],
+	}),
+	pack: {
+		deps: {
+			skipNodeModulesBundle: true,
+		},
+		dts: {
+			sourcemap: true,
+		},
+		entry: 'src/index.ts',
+		failOnWarn: true,
+		publint: true,
+		sourcemap: true,
+	},
+});

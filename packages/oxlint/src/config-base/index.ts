6d880ede165f72ea3ed85961fd585e55b30d34d0
Dom Porada <dom@dom.engineering>
2026-04-13 17:42:33 +0200

    Disable `jest/prefer-importing-jest-globals`

--- packages/oxlint/src/config-base/index.ts
+++ packages/oxlint/src/config-base/index.ts
@@ -210,6 +210,7 @@ const config: LinterConfigEntry = {
 		'jest/no-untyped-mock-factory': 'off',
 		'jest/prefer-called-with': 'off',
 		'jest/prefer-ending-with-an-expect': 'off',
+		'jest/prefer-importing-jest-globals': 'off',
 		'jest/prefer-lowercase-title': 'off',
 		'jest/prefer-snapshot-hint': 'off',
 		'jest/prefer-spy-on': 'off',

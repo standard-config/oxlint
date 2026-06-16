88d868af088f7fa04d91dff56a866c97ab98fa60
Dom Porada <dom@dom.engineering>
2026-06-16 10:48:55 +0200

    Disable `react/jsx-no-literals`

--- packages/oxlint-react/src/config-base/index.ts
+++ packages/oxlint-react/src/config-base/index.ts
@@ -86,6 +86,7 @@ const config: OxlintConfigBaseEntry = {
 		'react/jsx-no-comment-textnodes': 'error',
 		'react/jsx-no-constructed-context-values': 'error',
 		'react/jsx-no-duplicate-props': 'error',
+		'react/jsx-no-literals': 'off',
 		'react/jsx-no-script-url': 'error',
 		'react/jsx-no-target-blank': [
 			'error',
